36a53192cd71a7fcb889b69ec2b1114edb6f6560
Jason <j.hlavac@comcast.net>
2025-04-13 18:14:44 -0700

    findPassword Function
    Added a temporary button that calls the findPassword function that returns the password and password input element in a dictionary

--- aliaser.js
+++ aliaser.js
@@ -1,5 +1,16 @@
 function main(){
-    console.log("Hello World")
+    console.log('Hello World!');
+    var button = document.createElement("Button");
+    button.addEventListener('click', function(){
+        console.log(findPassword());
+    });
+    button.textContent = "Find Password";
+    document.body.prepend(button);
+};
+
+function findPassword(){
+    const passwordElement = document.querySelector('input[type= "password"]');
+    return({element: passwordElement, password: passwordElement.value});
 }
 
 main();
